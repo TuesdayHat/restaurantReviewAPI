9802573ac8d7a7175749e00e9b9120b6252f4de2
James Donlan <jacketlizard@live.com>
2018-01-23 09:31:40 -0800

    replaced mysteriously vanished code from yesterday

--- src/main/java/dao/RestaurantDao.java
+++ src/main/java/dao/RestaurantDao.java
@@ -1,7 +1,26 @@
 package dao;
 
-/**
- * Created by Guest on 1/22/18.
- */
+import models.Foodtype;
+import models.Restaurant;
+import models.Review;
+
+import java.util.List;
+
 public interface RestaurantDao {
+
+    //create
+    void add (Restaurant restaurant); //L
+    //void addRestaurantToFoodtype(Restaurant restaurant, Foodtype foodtype); //D
+
+    //read
+    List<Restaurant> getAll(); //A
+    // List<Foodtype> getAllFoodtypesByRestaurant(int restaurantId); //D - we will implement this soon.
+
+    Restaurant findById(int id); //B
+
+    //update
+    void update(int id, String name, String address, String zipcode, String phone, String website, String email); //N
+
+    //delete
+    void deleteById(int id); //M
 }
--- src/main/java/dao/ReviewDao.java
+++ src/main/java/dao/ReviewDao.java
@@ -1,7 +1,24 @@
 package dao;
 
-/**
- * Created by Guest on 1/22/18.
- */
+
+import models.Foodtype;
+import models.Restaurant;
+import models.Review;
+
+import java.util.List;
+
 public interface ReviewDao {
+
+    //create
+    void add(Review review); //G
+
+    //read
+    List<Review> getAllReviewsByRestaurant(int restaurantId); // J
+    List<Review> getAll(); // J
+
+    //update
+    //omit for now
+
+    //delete
+    void deleteById(int id); //O
 }
